cefc80fbf218b2fcb46c9c16746005a4d52f5195
ayarmarxio <ayarmarxio@gmail.com>
2018-11-23 00:03:30 +0100

    first commit

--- FakeBerthaPiSensor/Program.cs
+++ FakeBerthaPiSensor/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FakeBerthaPiSensor
@@ -10,6 +13,46 @@ namespace FakeBerthaPiSensor
     {
         static void Main(string[] args)
         {
+            UdpClient udpSender = new UdpClient(0) { EnableBroadcast = true };
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Broadcast, 7000);
+            Console.WriteLine("Broadcast ready. Get started Press Enter");
+            Console.ReadLine();
+            while (true)
+            {
+                RandomRecordGenerator _rand = new RandomRecordGenerator();
+
+                double _long = _rand.GetLong();
+                double _lat = _rand.GetLat();
+                double _bpSystolic = _rand.GetSystolic();
+                double _bpDiastolic = _rand.GetDiastolic();
+                double _bodyTemperature = _rand.GetTemperature();
+                int _heartBeatPerSecond = _rand.GetHeartBeat();
+                double _dust = _rand.GetDust();
+                double _sulphur = _rand.GetSulphur();
+                double _nitrogen = _rand.GetNitrogen();
+                double _fluor = _rand.GetFluor();
+                double _carbonMonoxide = _rand.GetCarbonMonoxide();
+                double _ozone = _rand.GetOzone();
+                int _userId = _rand.GetUserId();
+
+                string sensorData = $"long:{_long}-\r\n lat: {_lat}-\r\n bpsystolic:{_bpSystolic}-\r\n bpdiastolic: {_bpDiastolic}-\r\n bodytemperature{_bodyTemperature}-\r\n heartbeatpersecond{_heartBeatPerSecond}-\r\n dust:{_dust}-\r\n sulphur:{_sulphur}-\r\n nitrogen:{_nitrogen}-\r\n fluor:{_fluor}-\r\n carbonmonoxide:{_carbonMonoxide}-\r\n ozone:{_ozone}-\r\n userid:{_userId}-\r\n";
+
+                Byte[] sendBytes = Encoding.ASCII.GetBytes(sensorData);
+
+                try
+                {
+                    udpSender.Send(sendBytes, sendBytes.Length, endPoint);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+                Console.WriteLine("UserId:" + _userId);
+                Console.WriteLine("Long:" + _long);
+                Console.WriteLine("Lat:" + _lat);
+                Console.WriteLine("Broadcasting data on Port no: 7000");
+                Thread.Sleep(2000);
+            }
         }
     }
 }
---
+++ FakeBerthaPiSensor/RandomRecordGenerator.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeBerthaPiSensor
+{
+    public class RandomRecordGenerator
+    {
+        private Random _longRandom;
+        private Random _latRandom;
+        private Random _bpSystolicRandom;
+        private Random _bpDiastolicRandom;
+        private Random _bodyTemperatureRandom;
+        private Random _heartBeatPerSecondRandom;
+        private Random _dustRandom;
+        private Random _sulphurRandom;
+        private Random _nitrogenRandom;
+        private Random _fluorRandom;
+        private Random _carbonMonoxideRandom;
+        private Random _ozoneRandom;
+        private Random _userIdRandom;
+
+        public RandomRecordGenerator()
+        {
+            _longRandom = new Random();
+            _latRandom = new Random();
+            _bpSystolicRandom = new Random();
+            _bpDiastolicRandom = new Random();
+            _bodyTemperatureRandom = new Random();
+            _heartBeatPerSecondRandom = new Random();
+            _dustRandom = new Random();
+            _sulphurRandom = new Random();
+            _nitrogenRandom = new Random();
+            _fluorRandom = new Random();
+            _carbonMonoxideRandom = new Random();
+            _ozoneRandom = new Random();
+            _userIdRandom = new Random();
+        }
+
+        public double GetLong()
+        {
+            int _longBase = _longRandom.Next(1332450, 1406441);
+            double _longTail = _longRandom.NextDouble();
+            double _long = _longBase + _longTail;
+            return _long;
+        }
+
+        public double GetLat()
+        {
+            int _latBase = _latRandom.Next(7481656, 7574603);
+            double _latTail = _latRandom.NextDouble();
+            double _lat = _latBase + _latTail;
+            return _lat;
+        }
+
+        public double GetSystolic()
+        {
+            return _bpSystolicRandom.Next(95, 140);
+        }
+
+        public double GetDiastolic()
+        {
+            return _bpDiastolicRandom.Next(50, 85);
+        }
+
+        public double GetTemperature ()
+        {
+            return _bodyTemperatureRandom.Next(50, 85);
+        }
+        public int GetHeartBeat()
+        {
+            return _heartBeatPerSecondRandom.Next(60, 100);
+        }
+
+        public double GetDust()
+        {
+            return _dustRandom.Next(100);
+        }
+
+        public double GetSulphur()
+        {
+            return _sulphurRandom.NextDouble();
+        }
+
+        public double GetNitrogen()
+        {
+            return _nitrogenRandom.Next(10, 100);
+        }
+
+        public double GetFluor()
+        {
+            return _fluorRandom.Next(1, 10);
+        }
+
+        public double GetCarbonMonoxide()
+        {
+            return _carbonMonoxideRandom.Next(10, 200);
+        }
+
+        public double GetOzone()
+        {
+            return _ozoneRandom.Next(15, 90);
+        }
+
+        public int GetUserId ()
+        {
+            return _userIdRandom.Next(1, 5);
+        }
+
+
+    }
+}
